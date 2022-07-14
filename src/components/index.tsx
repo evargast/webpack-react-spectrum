6cc367b8bac89a34afb6cb2b6bc4b4ef48cde4e6
Eduardo Vargas <edvargast@gmail.com>
2022-07-14 10:25:50 -0500

    Update App index file

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 
-import App from "./App/App";
+import { App } from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
